16d90caca4f762400336c026f638aca5e2b53671
Connor Vosberg <connorjv@gmail.com>
2024-02-20 14:18:47 -0800

    create basic files for the app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Hi, I'm Connor Vosberg</h1>
+      <h2>Flatiron School Graduate - 2023</h2>
+      <h3>Full Stack Software Engineer based in Long Beach, California.</h3>
+    </div>
+  )
 }
