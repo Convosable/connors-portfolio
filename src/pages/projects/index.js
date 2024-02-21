60ae21d17dd992867e116c9f933c0417edd82686
Connor Vosberg <connorjv@gmail.com>
2024-02-20 17:24:59 -0800

    add projects folder with basic skeleton files

---
+++ src/pages/projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <h2>Projects I have created:</h2>
+      <p>insert links to project components?</p>
+    </div>
+  )
+}
+
+export default Projects;
---
+++ src/pages/projects/lbr_bouldering_league.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LbrBoudleringLeague = () => {
+  return (
+    <div>
+      <h1>LBR Boudlering League</h1>
+      <h2>Summary</h2>
+      <p>insert images for proj</p>
+      <p>insert link to github</p>
+      <p>insert link to active site</p>
+    </div>
+  )
+}
+
+export default LbrBoudleringLeague;
