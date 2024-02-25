f166d0cdc859a5e50478ada77d929ec6dfd0708e
Connor Vosberg <connorjv@gmail.com>
2024-02-25 10:23:56 -0800

    add styling component files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
+import { Link } from "gatsby";
 import * as React from "react"
 import Layout from "../components/Layout"
+import {header, btn} from '../styles/home.module.css';
 
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
-          <h1>Hi, I'm Connor Vosberg</h1>
-          <h2>Flatiron School Graduate - 2023</h2>
-          <h3>Full Stack Software Engineer based in Long Beach, California.</h3>
+          <h2>Learn, Develop, Deploy... Repeat</h2>
+          <h3>Flatiron School Graduate - 2023</h3>
+          <p>Full Stack Software Engineer based in Long Beach, California.</p>
+          <Link className={btn} to='/projects'>My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Layout from '../../components/Layout';
+import {portfolio} from '../../styles/projects.module.css';
 
 const Projects = () => {
   return (
     <Layout>
-      <div>
-        <h1>Portfolio</h1>
-        <h2>Projects I have created:</h2>
+      <div className={portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects I have created:</h3>
         <p>insert links to project components</p>
       </div>
     </Layout>
