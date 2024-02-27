f6a8f5c58d3937c98fe2f355b8e8c44167038645
Connor Vosberg <connorjv@gmail.com>
2024-02-27 08:40:12 -0800

    add linkedIn icon within a link in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,17 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 
 const Navbar = ( {icons} ) => {
-  const github = icons.nodes[0].childImageSharp.gatsbyImageData
+  const github = icons.nodes[1].childImageSharp.gatsbyImageData
+  const linkedIn = icons.nodes[0].childImageSharp.gatsbyImageData
   return (
     <nav>
         <div className='icons'>
           <h1>Connor Vosberg</h1>
             <a href='https://github.com/Convosable' target="_blank" rel="noopener noreferrer">
               <GatsbyImage image={getImage(github)} alt="github-icon" />
+            </a>
+            <a href='https://www.linkedin.com/in/connor-vosberg/' target="_blank" rel="noopener noreferrer">
+              <GatsbyImage image={getImage(linkedIn)} alt="linkedIn-icon" />
             </a>
         </div>
         <div className='links'>
