14a69e33f207c53b1ab67ff96a080fadbfd3492f
Connor Vosberg <connorjv@gmail.com>
2024-03-03 11:27:50 -0800

    update home page css to be compatible wtih smaller screens

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../components/Layout"
-import {header, btn} from '../styles/home.module.css';
+import {header} from '../styles/home.module.css';
 
 
 export default function Home( {data} ) {
@@ -10,13 +10,12 @@ export default function Home( {data} ) {
   const profileImage = data.profileImage.childImageSharp.gatsbyImageData;
 
   return (
-    <Layout >
+    <Layout>
       <section className={header}>
         <div>
           <h2>Learn, Develop, Deploy... Repeat</h2>
-          <h3>Flatiron School Graduate - 2023</h3>
+          <h3>Flatiron School Graduate</h3>
           <p>Full Stack Software Engineer based in Long Beach, California.</p>
-          <Link className={btn} to='/projects'>My Portfolio Projects</Link>
         </div>
         <GatsbyImage image={getImage(profileImage)} alt='connor-pic' />
       </section>
@@ -26,7 +25,7 @@ export default function Home( {data} ) {
 
 export const Head = () => (
   <>
-    <meta charset="UTF-8"/>
+    <meta charSet="UTF-8"/>
     <meta name="description" content="Connor Vosberg's professional protfolio page."></meta>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Connor Vosberg - Portfolio</title>
